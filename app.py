6ebdbd971cbe68a07d35c3df178099d771062b7d
ajmalpk23 <61465738+ajmalpk23@users.noreply.github.com+ajmalpk23@gmail.com>
2020-09-25 15:52:20 +0530

    admin and owner updated

--- app.py
+++ app.py
@@ -65,28 +65,68 @@ def admin_home():
 @app.route('/view_pending_workshop')
 def view_pending_workshop():
     db =Db()
-    res=db.select("SELECT `workshop`.*,`login`.username FROM `workshop`,`login` WHERE `workshop`.shop_id=`login`.login_id AND user_type='pending'")
+    res=db.select("SELECT workshop.* FROM login,workshop WHERE login.user_type='pending' AND login.login_id=workshop.login_id ")
     return render_template('admin/view_pending_workshop.html',data=res)
+
+@app.route('/approve_workshop/<wid>')
+def approve_workshop(wid):
+    db=Db()
+    res=db.selectOne("SELECT workshop.* FROM workshop WHERE  workshop.login_id='"+wid+"'")
+    session['wlid']=wid
+    return render_template('admin/approve_worckshop.html',data=res)
+
+@app.route('/approve_workshop_post',methods=['post'])
+def approve_workshop_post():
+    db = Db()
+    wid=session['wlid']
+    db.update("UPDATE login SET user_type='owner' WHERE login_id='"+wid+"'")
+    return '<script>alert("successfully approved");window.location="/view_pending_workshop"</script>'
+
 
 @app.route('/view_approved_workshop')
 def view_approved_workshop():
-    return render_template('admin/view_approved_workshop.html')
+    db = Db()
+    res = db.select(
+        "SELECT workshop.* FROM login,workshop WHERE login.user_type='owner' AND login.login_id=workshop.login_id ")
+    return render_template('admin/view_approved_workshop.html',data=res)
+
+
+@app.route('/more_info/<wid>')
+def more_info(wid):
+    db = Db()
+    res=db.selectOne("SELECT workshop.* FROM workshop WHERE  workshop.login_id='"+wid+"' ")
+    res2=db.select("SELECT * FROM services WHERE shop_id='"+wid+"'")
+    return  render_template('admin/more_info.html',data=res,data2=res2)
+
+
 
 @app.route('/view_rejected_workshop')
 def view_rejected_workshop():
-    return render_template('admin/rejected_workshop.html')
+    db = Db()
+    res = db.select(
+        "SELECT workshop.* FROM login,workshop WHERE login.user_type='rejected' AND login.login_id=workshop.login_id ")
+    return render_template('admin/rejected_workshop.html',data=res)
+
+
+
 
 @app.route('/Manage_complaints')
 def Manage_complaints():
-    return render_template('admin/view_complaints.html')
+    db = Db()
+    res=db.select("SELECT complaint.*,user.name FROM complaint,USER WHERE complaint.user_id=user.login_id ORDER BY complaint.complaint_id DESC")
+    return render_template('admin/view_complaints.html',data=res)
 
 @app.route('/View_feedback')
 def View_feedback():
-    return render_template('admin/view_feedback.html')
+    db = Db()
+    res=db.select("SELECT feedback.*,user.name FROM USER,feedback  WHERE feedback.user_id=user.user_id ORDER BY feedback.feedback_id DESC")
+    return render_template('admin/view_feedback.html',data=res)
 
 @app.route('/View_rating')
 def View_rating():
-    return render_template('admin/view rating.html')
+    db =Db()
+    res=db.select("SELECT rating.*,workshop.shop_name,user.name FROM rating,workshop,USER,service_request WHERE rating.service_request_id=service_request.service_request_id AND service_request.user_id=user.login_id AND service_request.workshop_id=workshop.login_id ORDER BY rating.rating_id DESC")
+    return render_template('admin/view rating.html',data=res)
 
 @app.route('/Add_Notification')
 def Add_Notification():
@@ -106,7 +146,7 @@ def Add_Notification_post():
 @app.route('/View_Notification')
 def View_Notification():
     db=Db()
-    res=db.select("SELECT * FROM notification ORDER BY notification.notification_id DESC")
+    res=db.select("SELECT * FROM notification ORDER BY notification.notification_id desc ")
     return render_template('admin/view_Notification.html',data=res)
 
 @app.route('/Add_news')
@@ -128,12 +168,45 @@ def Add_news_post():
 @app.route('/view_news')
 def view_news():
     db=Db()
-    res=db.select("SELECT * FROM news ORDER BY news.news_id DESC")
+    res=db.select("SELECT * FROM news ORDER BY news.news_id desc ")
     return render_template('admin/view news.html',data=res)
+
+@app.route('/edit_news/<nid>')
+def edit_news(nid):
+    db=Db()
+    res=db.selectOne("SELECT * FROM news WHERE news_id='"+nid+"'")
+    session['neid']=nid
+    return render_template('admin/edit news.html',data=res)
+
+@app.route('/edit_news_post',methods=['post'])
+def edit_news_post():
+    title = request.form['textfield']
+    discription = request.form['textarea']
+    files = request.files['fileField']
+    nid = session['neid']
+    db = Db()
+    file_name = 'news_' + str(nid) + '.jpg'
+    files.save(static_path + "news\\" + file_name)
+    db.update("UPDATE news SET DATE=CURDATE(),news_title='"+title+"',news_description='"+discription+"',image='"+file_name+"' WHERE news_id='"+nid+"'")
+    return '<script>alert("successfully updated");window.location="/view_news"</script>'
+
 
 @app.route('/change_password')
 def change_password():
     return render_template('admin/change_password.html')
+
+@app.route('/change_password_post',methods=['post'])
+def change_password_post():
+    cr_pass=request.form['password']
+    new_pass=request.form['password2']
+    lid = str(session['lid'])
+    db =Db()
+    res = db.selectOne("SELECT * FROM login WHERE login_id='" + lid + "' AND password='" + cr_pass + "'")
+    if res is not None:
+        db.update("UPDATE login SET password='" + new_pass + "' WHERE login_id='" + lid + "'")
+        return '<script>alert("successfully updated");window.location="/"</script>'
+    else:
+        return '<script>alert("invalid current password");window.location="/change_password"</script>'
 
 
 ###############owner###############
@@ -143,15 +216,56 @@ def owner_home():
 
 @app.route('/view_profile')
 def view_profile():
-    return render_template('owner/view profile.html')
+    db = Db()
+    lid =str(session['lid'])
+    res=db.selectOne("SELECT * FROM  workshop WHERE login_id='"+lid+"'")
+    return render_template('owner/view profile.html',data=res)
+
+@app.route('/update_view_profile/',methods=['post'])
+def update_view_profile():
+    shop_name = request.form['textfield5']
+    place = request.form['textfield2']
+    city = request.form['textfield3']
+    district = request.form['textfield4']
+    pincode = request.form['number']
+    emial = request.form['email']
+    files = request.files['file']
+    phone = request.form['number2']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+    db =Db()
+    lid = str(session['lid'])
+    file_name = 'worckshop_' + str(lid) + '.jpg'
+    files.save(static_path + "worckshop\\" + file_name)
+
+    db.update("UPDATE  workshop SET shop_name='"+shop_name+"',place='"+place+"',city='"+city+"',district='"+district+"',pincode='"+pincode+"',email='"+emial+"',shop_lisence='"+file_name+"',phone='"+phone+"',lati='"+latitude+"',longi='"+longitude+"' WHERE login_id='"+lid+"'")
+    db.update("UPDATE login SET username='"+emial+"' WHERE login_id='"+lid+"'")
+    return '<script>alert("updated successfully");window.location="/view_profile"</script>'
+
 
 @app.route('/change_owner_password')
 def change_owner_password():
     return render_template('owner/change_owner_password.html')
+@app.route('/change_owner_password_post',methods=['post'])
+def change_owner_password_post():
+    cr_pass=request.form['password']
+    new_pass=request.form['password2']
+    lid = str(session['lid'])
+    db =Db()
+    res = db.selectOne("SELECT * FROM login WHERE login_id='" + lid + "' AND password='" + cr_pass + "'")
+    if res is not None:
+        db.update("UPDATE login SET password='" + new_pass + "' WHERE login_id='" + lid + "'")
+        return '<script>alert("successfully updated");window.location="/"</script>'
+    else:
+        return '<script>alert("invalid current password");window.location="/change_owner_password"</script>'
+
 
 @app.route('/gallery_management')
 def gallery_management():
-    return render_template('owner/gallery_management.html')
+    db =Db()
+    lid = str(session['lid'])
+    res=db.select("SELECT * FROM gallery WHERE shop_id='"+lid+"'")
+    return render_template('owner/gallery_management.html',data=res)
 
 @app.route('/add_photo')
 def add_photo():
@@ -171,7 +285,29 @@ def add_photo_post():
 
 @app.route('/view_services')
 def view_services():
-    return render_template('owner/view service.html')
+    db = Db()
+    lid = str(session['lid'])
+    res=db.select("SELECT * FROM  services WHERE shop_id='"+lid+"' ORDER BY services.service_id DESC")
+    return render_template('owner/view service.html',data=res)
+
+@app.route("/edit_service/<sid>")
+def edit_service(sid):
+    db = Db()
+    res=db.selectOne("SELECT * FROM services WHERE service_id='"+sid+"'")
+    session['ab']=sid
+    return render_template('owner/edit service.html',data=res)
+
+@app.route('/edit_service_post',methods=['post'])
+def edit_service_post():
+    db = Db()
+    service = request.form.get('filde')
+    vehicle_type = request.form.get('service')
+    price = request.form['price']
+
+    sid=str(session['ab'])
+    db.update("UPDATE services SET service='"+service+"',vehichle_type='"+vehicle_type+"',amount='"+price+"' WHERE service_id='"+sid+"'")
+    return '<script>alert("successfully updated");window.location="/view_services"</script>'
+
 
 @app.route('/add_services')
 def add_services():
@@ -193,19 +329,37 @@ def add_services_post():
 
 @app.route('/pending_service_requestes')
 def pending_service_requestes():
-    return render_template('owner/view pending requests.html')
+    lid = str(session['lid'])
+    db =Db()
+    res=db.select("SELECT service_request.*,user.* FROM workshop,USER,service_request WHERE service_request.user_id=user.login_id AND service_request.workshop_id=workshop.login_id AND workshop.login_id='"+lid+"' AND service_request.status='pending' ORDER BY service_request.service_request_id DESC")
+    return render_template('owner/view pending requests.html',data=res)
+
+@app.route('/more_info_pending_request/<sid>')
+def more_info_pending_request(sid):
+    db =Db()
+    res=db.selectOne("")
 
 @app.route('/approved_service_requestes')
 def approved_service_requestes():
-    return render_template('owner/view approved service requests.html')
+    lid = str(session['lid'])
+    db =Db()
+    res=db.select("SELECT service_request.*,user.* FROM workshop,USER,service_request WHERE service_request.user_id=user.login_id AND service_request.workshop_id=workshop.login_id AND workshop.login_id='"+lid+"' AND service_request.status='approved' ORDER BY service_request.service_request_id DESC")
+    return render_template('owner/view approved service requests.html',data=res)
 
 @app.route('/service_request_history')
 def service_request_history():
-    return render_template('owner/view service requests history.html')
+    lid = str(session['lid'])
+    db = Db()
+    res=db.select("SELECT service_request.*,user.* FROM workshop,USER,service_request WHERE service_request.user_id=user.login_id AND service_request.workshop_id=workshop.login_id AND workshop.login_id='"+lid+"' AND service_request.status='done' ORDER BY service_request.service_request_id DESC")
+    return render_template('owner/view service requests history.html',data=res)
 
 @app.route('/view_rating')
 def view_rating():
-    return render_template('owner/view rating.html')
+    lid = str(session['lid'])
+    db =Db()
+    res=db.select("SELECT rating.*,user.name,user.place FROM rating,workshop,USER,service_request WHERE rating.service_request_id=service_request.service_request_id AND service_request.user_id=user.login_id AND service_request.workshop_id=workshop.login_id AND workshop.login_id='"+lid+"' ORDER BY rating.rating_id DESC")
+    return render_template('owner/view rating.html',data=res)
+
 
 
 
